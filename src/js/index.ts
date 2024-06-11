710b7aa194caed847b0c6d9786d7bcdd5d6f0687
Savus <justin.fuzzygreenstuff@gmail.com>
2024-06-11 05:35:35 -0600

    refactor typewriter animation

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,25 +7,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const typingSlot = document.querySelector(".typing-slot") as HTMLElement;
 
   const phrases = [
-    "code",
+    "This is another long conversation that I'm writting out simply to see how fast the text would scroll and how fast I need to shorten the animation delay",
     "hug puppies",
     "play magic",
     "So here is a conversation phrase that you might see scrolling across your screen at high speed",
   ];
 
-  const animationSpeed = 10;
+  const animationSpeed = 0.5;
 
   let phraseIndex = 0;
 
-  const cycleThroughPhrases = async () => {
-    while (phraseIndex < phrases.length) {
+  const cycleThroughPhrases = async (phrases: string[]) => {
+    while (phraseIndex <= phrases.length - 1) {
       const currentPhrase = phrases[phraseIndex];
       for (let i = 0; i <= currentPhrase.length - 1; i++) {
         typingSlot.innerText = currentPhrase.substring(0, i + 1);
         await sleep(animationSpeed);
       }
 
-      await sleep(animationSpeed + 500);
+      await sleep(animationSpeed + 1000);
 
       for (let i = currentPhrase.length - 1; i > 0; i--) {
         typingSlot.innerText = currentPhrase.substring(0, i - 1);
@@ -38,5 +38,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  cycleThroughPhrases();
+  cycleThroughPhrases(phrases);
 });
