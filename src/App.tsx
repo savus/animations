8aaf33eeef1ffd357acabb83da87cdfe56e60231
Savus <justin.fuzzygreenstuff@gmail.com>
2024-06-11 04:42:14 -0600

    build scrolling automatic typewriter function for text effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "../src/css/main.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="typewriter-container">
+        <div className="typewriter-text">
+          I love to <span className="typing-slot">Example</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const sleep = (ms: number) =>
+    new Promise((resolve) => {
+      return setTimeout(resolve, ms);
+    });
+
+  const typingSlot = document.querySelector(".typing-slot") as HTMLElement;
+
+  const phrases = [
+    "code",
+    "hug puppies",
+    "play magic",
+    "So here is a conversation phrase that you might see scrolling across your screen at high speed",
+  ];
+
+  const animationSpeed = 10;
+
+  let phraseIndex = 0;
+
+  const cycleThroughPhrases = async () => {
+    while (phraseIndex < phrases.length) {
+      const currentPhrase = phrases[phraseIndex];
+      for (let i = 0; i <= currentPhrase.length - 1; i++) {
+        typingSlot.innerText = currentPhrase.substring(0, i + 1);
+        await sleep(animationSpeed);
+      }
+
+      await sleep(animationSpeed + 500);
+
+      for (let i = currentPhrase.length - 1; i > 0; i--) {
+        typingSlot.innerText = currentPhrase.substring(0, i - 1);
+        await sleep(animationSpeed);
+      }
+
+      await sleep(animationSpeed + 500);
+
+      phraseIndex++;
+    }
+  };
+
+  cycleThroughPhrases();
+});
